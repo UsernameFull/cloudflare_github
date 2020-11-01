410875b51c0536732c9f4e8040ffd5db51b2853c
Username_Full <tohowtodoit@gmail.com>
2020-11-02 00:24:25 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,16 +2,20 @@ const html = `<!DOCTYPE html>
 <body>
 <h1>GitHub release加速</h1>
 <p>在下方填写你需要下载的release连接(例如https://github.com/microsoft/vscode/archive/1.49.3.zip)，然后点击下载即可。</p>
+<p>ps：理论上只要是直链都可以做加速，但本人未作测试，不保证效果</p>
 <p>本项目基于cloudflare worker，仅用于个人下载测试使用，切勿滥用，需要大批量下载的用户请参考本项目<a href="https://github.com/UsernameFull/cloudflare_github">GitHub</a></p>
 <input type="text" id="input"/>
 <button id="Btn">下载</button>
 </body>
 <script type="text/javascript">
-document.getElementById("Btn").addEventListener("click", function(){
+function download (){
+
 if(document.getElementById("input").value){
 var a = document.createElement("a");
-a.setAttribute("href", "https://noisy-poetry-f4ed.tohowtodoit.workers.dev/?"+document.getElementById("input").value);
+let downloadUrl = document.getElementById("input").value
+a.setAttribute("href", window.location+"?"+downloadUrl);
 a.setAttribute("target", "_blank");
+a.download =downloadUrl.split("/").slice(-1);
 a.setAttribute("id", "startTelMedicine");
 // 防止反复添加
 if(document.getElementById("startTelMedicine")) {
@@ -20,6 +24,18 @@ if(document.getElementById("startTelMedicine")) {
 document.body.appendChild(a);
 a.click();
 }
+
+}
+document.body.addEventListener('keyup', function (e) {
+var theEvent = window.event || e;
+var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+if (code == 13) {
+download()
+}
+})
+
+document.getElementById("Btn").addEventListener("click", function(){
+download()
 });
 </script>
 `
@@ -34,11 +50,9 @@ async function handleRequest(request) {
 
 addEventListener("fetch", event => {
     console.log(event.request)
-    if(event.request.url=="https://noisy-poetry-f4ed.tohowtodoit.workers.dev/"){
-        console.log("11111111111")
+    if(!event.request.url.split("?")[1]){
         return event.respondWith(handleRequest(event.request))
     }
-    event.request.url="https://noisy-poetry-f4ed.tohowtodoit.workers.dev"
     event.respondWith(fetchAndApply(event.request));
 })
 
